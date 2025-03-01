da93d82e0126fe12a1257c968af0f90969876725
Galaceon <antoniogarcialtr@gmail.com>
2025-03-01 18:55:28 +0100

    01 finished

---
+++ js/01.js
@@ -0,0 +1,28 @@
+// VARIABLES
+
+var producto = 'Audifonos Gamer'; // Iniciar variable y asignarle valor
+
+var disponible; // Iniciamos la variable pero sin valor
+
+producto = true; // Reasignamos el valor de la variable
+
+disponible = true;
+
+var producto1 = 'Computadora', 
+    disponible1 = true,
+    categoria = 'Computadoras';
+
+
+// var 1disponible; // No es posible iniciar una variable con numeros
+// var -disponible; //No es posible iniciar una variable con guiones
+
+var disponible1; // El numero puede ir en todo el nombre menos al principio
+var _disponible_; //Si es posible usar barra baja donde sea
+
+// Estilos para las variables
+var nombre_producto = 'Monitor HD'; // underscore - Segundo mas usado
+var nombreProducto = 'Monitor HD'; // Camelcase - Mas usado
+var NombreProducto = 'Monitor HD'; // Pascal Case
+var nombreproducto = 'MonitorHD'; // lowerCase
+
+console.log(PRODUCTO); //no funcionara ya que la variable fue creada en minusculas
