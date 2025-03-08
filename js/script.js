1569acb62143eedd675d89069fac317a59abe0f1
Galaceon <antoniogarcialtr@gmail.com>
2025-03-08 21:37:50 +0100

    script updated, POO, THIS, forEach and Map

--- js/script.js
+++ js/script.js
@@ -18,9 +18,66 @@ const producto = {
     precio : 30
 }
 
-const newArray = [...arrayDeNumeros, ...PCPiezas]
 
-console.log(newArray)
+
+function Juego(nombre, precio, disponible) {
+    this.nombre = nombre;
+    this.precio = precio;
+    this.disponible = disponible;
+}
+
+const darkSouls = new Juego ('Dark souls', 30, true);
+const hollowKnight = new Juego ('Hollow Knight', 15, false);
+
+function formatearJuego(juego) {
+    return `El juego ${juego.nombre} vale $ ${juego.precio} y esta ${juego.disponible}`;
+}
+
+console.log(formatearJuego(darkSouls))
+
+// console.log(darkSouls);
+// console.log(hollowKnight);
+
+
+
+/** THIS **/
+// const obj = {
+//     name : 'Anto',
+//     normalFunction: function() {
+//         console.log(this.name);
+//     },
+//     arrowFunction: () => {
+//         console.log(this.name);
+//     }
+// }
+// obj.normalFunction();
+// obj.arrowFunction();
+
+
+
+/** forEach & Map || Mostrar en consola y imprimir en HTML && Crear nuevos arrays con los parametros deseados **/
+// const yokse1 = animales.forEach( animal => console.log(`animal seria: ${animal.especie} y lo que vive seria: ${animal.añosVivo}`));
+// const animalada2 = animales.map( animal => `animal: ${animal.especie} - añosQueVive: ${animal.añosVivo}`)
+// console.log(animalada2)
+
+
+
+/** WHILE LOOP **/
+// let i = 1;
+// while(i <= 10) {
+//     console.log(i);
+//     console.log('buena');
+
+//     i++;
+// }
+
+
+/** SPREAD OPERATOR || JUNTA ARRAYS O OBJETOS EN UNO SOLO **/
+// const newArray = [...arrayDeNumeros, ...PCPiezas]
+
+// console.log(newArray)
+
+
 
 /** CREAR MIS PROPIOS METODOS **/
 // const reproductor = {
