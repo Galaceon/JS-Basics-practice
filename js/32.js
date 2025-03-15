90b56ed55f1317213c9614813c5e4d36f62d9525
Galaceon <antoniogarcialtr@gmail.com>
2025-03-15 23:42:20 +0100

    32 modified

--- js/32.js
+++ js/32.js
@@ -1,40 +1,32 @@
-// Async / await
+Async / await
 
-function descargarNuevosClientes() {
-    return new Promise( resolve => {
-        console.log('Descargando clientes... espere...');
+function descargarClientes(callback) {
+    
+    console.log('Se estan descargando los clientes')
 
-        setTimeout( () => {
-            resolve('Los Clientes fueron Descargados');
-        }, 5000 );
-    });
+    setTimeout( () => {
+        callback('Los clientes han sido descargados')
+    }, 5000)
 }
 
-function descargarUltimosPedidos() {
-    return new Promise( resolve => {
-        console.log('Descargando pedidos... espere...');
 
-        setTimeout( () => {
-            resolve('Los Pedidos fueron Descargados');
-        }, 3000 );
-    });
-}
+// function descargarNuevosClientes() {
+//     return new Promise( resolve => {
+//         console.log('Descargando Clientes... Espere...')
+
+//         setTimeout( () => {
+//             resolve('Los clientes fueron descargados')
+//         }, 5000)
+//     })
+// }
 
 async function app() {
-   try {
-    //    const clientes = await descargarNuevosClientes();
-    //    const pedidos = await descargarUltimosPedidos();
-    //    console.log(clientes);
-    //    console.log(pedidos);
-
-    const resultado = await Promise.all([ descargarNuevosClientes(), descargarUltimosPedidos() ]);
-    console.log(resultado[0]);
-    console.log(resultado[1]);
-   } catch (error) {
-       console.log(error);
-   }
-
-
+    try {
+        const resultado = await descargarClientes();
+        console.log(resultado);
+    } catch (error) {
+        console.log(error)
+    }
 }
 
 app();
